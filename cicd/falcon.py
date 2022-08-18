e66a2cc5dfe919ae6d775a13cee7d886887fbdbc
Bala <mailtobalas@gmail.com>
2022-08-18 23:38:24 +0530

    jenkings update

--- cicd/falcon.py
+++ cicd/falcon.py
@@ -12,15 +12,13 @@ import uuid
 REGISTRY = "euwppe127cosbuilddeployacr.azurecr.io"
 
 PROFILES_LIST = [
-    'kafkatofolder',
-    'kafkatofolders'
+    'kafkatofolder'
 ]
 
 # key --> starts with
 # value --> profile
 tree = {
     "kafkatofolder":"kafkatofolder",
-    "kafkatofolders":"kafkatofolders",
     "pom.xml": "all",
     "jenkins": "all"
 }
